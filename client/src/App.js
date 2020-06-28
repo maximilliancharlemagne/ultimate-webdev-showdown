f49040a378d7116756b5463358cde1935e7561b6
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-06-28 09:34:02 -0400

    installed react-router, mui-core, mui-icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//import pages
+import Game from './pages/Game'
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Switch>
+        <Route path = "/">
+          {/* Putting the game page in root for testing purposes */}
+          <Game/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
