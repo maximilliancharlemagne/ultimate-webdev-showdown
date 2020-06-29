aa48dd6244e12db8aff56a26fae1cc01cf0f267d
Isaac Householder <isaachouseholder@gmail.com>
2020-06-29 14:45:01 -0700

    Socket.io intial setup
    - it can display x and o, alternating on alternating browsers.

--- server.js
+++ server.js
@@ -2,11 +2,48 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const server = require('html')(app)
+const http = require('http')
+const server = http.createServer(app)
 const path = require('path')
 
 express.static(path.join(__dirname,'client','build'))
 
+// Socket.io server listens to our app
+let io = require('socket.io').listen(server);
+
+// Send current time to all connected clients
+function sendTime() {
+    io.emit('time', { time: new Date().toJSON() });
+}
+
+// Send current time every 10 secs
+setInterval(sendTime, 3000);
+
+
+let x = true;
+
+
+function xoChooser() {
+  let piece = x? "x" : "o"
+  x = !x
+  return piece
+}
+
+
+
+
+// Emit welcome message on connection
+io.on('connection', function(socket) {
+
+  test = xoChooser()
+   
+    // Use socket to communicate with this particular client only, sending it it's own id
+    socket.emit('welcome', { message: test });
+    
+    socket.on('i am client', console.log);
+});
+
+
 server.listen(process.env.PORT, () => {
-  console.log(`http://localhost${process.env.PORT}`)
+  console.log(`http://localhost:${process.env.PORT}`)
 })
