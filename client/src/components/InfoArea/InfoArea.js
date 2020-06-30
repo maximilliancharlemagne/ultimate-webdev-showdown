f95f95ff94ea0a0c38eaabebfd059f60511919a8
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-06-30 17:30:40 -0400

    added componentDidMount to InfoArea

--- client/src/components/InfoArea/InfoArea.js
+++ client/src/components/InfoArea/InfoArea.js
@@ -6,25 +6,34 @@ import Divider from '@material-ui/core/Divider'
 //import custom components
 import PlayerCard from '../PlayerCard'
 
-const InfoArea = (props) => {
-  return (
-    <>
-      <h1>Info Area</h1>
-      <PlayerCard num="1" playerName="Ben Roznos" leaderBoardPos="1" />
-      <PlayerCard num="2" playerName="Kele Palafox" leaderBoardPos="2" />
+class InfoArea extends React.Component {
+  componentDidMount() {
+    //do some stuff
+    //with axios
 
-      <Divider />
+    //¯\_( ツ )_/¯
+  }
+  
+  render() {
+    return (
+      <>
+        <h1>Info Area</h1>
+        <PlayerCard num="1" playerName="Ben Roznos" leaderBoardPos="1" />
+        <PlayerCard num="2" playerName="Kele Palafox" leaderBoardPos="2" />
 
-      <h2>{props.currentPlayer}'s Turn</h2>
+        <Divider />
 
-      <Divider />
+        <h2>###'s Turn</h2>
 
-      <h2>Score</h2>
-      <h3>Player 1: </h3>
-      <h3>Player 2: </h3>
-      <h3>Draws: </h3>
-    </>
-  );
+        <Divider />
+
+        <h2>Score</h2>
+        <h3>Player 1: </h3>
+        <h3>Player 2: </h3>
+        <h3>Draws: </h3>
+      </>
+    );
+  }
 }
 
 export default InfoArea
