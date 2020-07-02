22d9d1cac5b3c062cc38e39ef8a2c8502e2d3c10
Ben Roznos <benrozos@gmail.com>
2020-07-02 14:07:31 -0700

    added put and delete routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,14 @@ router.get('/users/',passport.authenticate('jwt'),(req,res) => {
   res.json(req.user)
 })
 
+// change user's avatar picture
+router.put('/users/updatePicture',passport.authenticate('jwt'),(req,res)=>{
+  User.findByIdAndUpdate(req.user._id, { userAvatarImageLink: req.body.newLink},{new:true},(err,user)=>{
+    res.json(user)
+  })
+})
+
+
 // sign in funtionality
 router.post('/users/signUp',(req, res)=>{
   const{ username, leaderboardRanking, userAvatarImageLink } = req.body
@@ -26,4 +34,14 @@ router.post('/users/signUp',(req, res)=>{
     })
   })
 
+  // delete User
+  router.delete('/users/deleteUser',passport.authenticate('jwt'),(req,res)=>{
+    User.findByIdAndDelete(req.user._id, (err,user)=>{
+      console.log(err)
+      res.sendStatus(200)
+    })
+    
+  })
+
+  //
 module.exports = router
