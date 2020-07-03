4cea67a3b4d97bbfdac56c380d2b02f102d81dbb
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-07-03 14:05:21 -0400

    playercard changes

--- client/src/components/PlayerCard/PlayerCard.js
+++ client/src/components/PlayerCard/PlayerCard.js
@@ -1,17 +1,22 @@
+//import CSS
 import React from 'react';
 
 //import mui-core components
 import Card from '@material-ui/core/Card'
+import Avatar from '@material-ui/core/Avatar'
+
+//import CSS
+import './PlayerCard.css'
 
 const PlayerCard = (props) => {
   return (
-    <Card variant = "outlined" style = {{display: "inline-block"}}>
+    <Card variant="outlined" style={{ display: "inline-block" }}>
       <h2>Player {props.num}</h2>
-      <img src = {props.imgSrc} alt = "player avatar"/>
+      <Avatar alt="Avatar" src={props.imgSrc} className="avatar" />
       <h3>{props.playerName}</h3>
       <h3>Leaderboard Rank: {props.leaderBoardPos}</h3>
     </Card>
-  )
+  );
 }
 
 export default PlayerCard
