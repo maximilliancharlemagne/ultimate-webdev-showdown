7a9b1bc2257142736d71ee17e38209f1030e0bb7
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-07-03 14:47:00 -0400

    socket io fix?

--- client/src/utils/SocketAPI/SocketAPI.js
+++ client/src/utils/SocketAPI/SocketAPI.js
@@ -1,5 +1,5 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:3001");
+const socket = openSocket();
 
 function listenToBoard(cb) {
   socket.on('boardEvent', (data) => {
